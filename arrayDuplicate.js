468a9e34a5f0a8710819efca78eafdcebe714a81
Hema35 <cheershema@gmail.com>
2023-02-12 14:10:57 -0600

    Removing the duplicates

--- arrayDuplicate.js
+++ arrayDuplicate.js
@@ -1,5 +1,6 @@
 const duplicateArray = [14,5,6,14,3,6,7,9];
+function uniqueList(duplicateArray){
+    return [...new Set(duplicateArray)];
+}
 
-const duplicates = duplicateArray.filter((ele, index, arr) => arr.indexOf(ele) !== index);
-
-console.log('ele:',duplicates);
+console.log(uniqueList(duplicateArray)); //[14,5,6,3,7,9]
